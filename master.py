ec4a858261526e9bd5af64c7390d43ed7358df30
Roksolana Povalii <rpovalii@tripadvisor.com>
2023-10-10 13:04:15 +0300

    added flask

--- master.py
+++ master.py
@@ -1,24 +1,62 @@
 from concurrent import futures
 from threading import Thread
-from protocol import Protocol
+from flask import Flask, request
+from flask_restful import Resource, Api
 
 import logging
 import grpc
 import duplicatedlog_pb2 as my_pb   
 import duplicatedlog_pb2_grpc as my_grpc
 import time
+import uuid
+
+app = Flask(__name__)
+api = Api(app)
 
 logging.basicConfig(format='%(asctime)s: : %(message)s', level=logging.DEBUG)
 log = logging.getLogger(__name__)
-protocol = Protocol()
-       
-def send_message(msg):
-    log.info("Sending a message ...")
+
+messages = {}
+masterPort = 50051
+
+class MasterProtocol(my_grpc.ProtocolServicer):
+    nodes = {}
+    port = masterPort
+
+    def register_secondary_node(self, request, context):
+        self.port += 1
+        self.nodes[request.message] = self.port
+        log.info(f"Registering secondary node on port: {self.port}")
+        return my_pb.Response(message=str(self.port))
+
+class MessageApi(Resource):
+    def get(self):
+        return messages
+
+    def post(self):
+        key = str(uuid.uuid4())
+        messages[key] = request.form['message']
+        replicate(request.form['message'])
+        return key
+
+api.add_resource(MessageApi, '/')
+protocol = MasterProtocol()
+
+def replicate(msg):
+    log.info(f"Sending a message ... {msg}")
+    messages[str(uuid.uuid4())] = msg
+
     for key in protocol.nodes:
         with grpc.insecure_channel("localhost:" + str(protocol.nodes[key])) as channel:
             stub = my_grpc.ProtocolStub(channel)
             response = stub.replicate(my_pb.Request(message=msg))
         log.info(f"Response from {key}: {response.message}")
+
+def test_replication():
+    # test message replication
+    for count in range(5):
+        time.sleep(3)
+        replicate("message" + str(count))
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -28,18 +66,13 @@ def serve():
     log.info(f"Server started, listening on port: {protocol.port}")
     server.wait_for_termination()
 
+def web():
+    app.run(debug=True, use_reloader=False, port=masterPort)
 
 if __name__ == "__main__":
-    t = Thread(target=serve)
-    t.start()
-    for count in range(5):
-        time.sleep(3)
-        send_message("message" + str(count))
+    Thread(target=web).start()
+    # Thread(target=test_replication).start()
+    serve()
 
-    t.join()
 
-    
 
-# TODO: 
-# flask add message/ list messages
-# store messages in memory
--- secondary.py
+++ secondary.py
@@ -1,5 +1,7 @@
 from concurrent import futures
-from protocol import Protocol
+from threading import Thread
+from flask import Flask
+from flask_restful import Resource, Api
 
 import logging
 import uuid
@@ -7,21 +9,37 @@ import grpc
 import duplicatedlog_pb2 as my_pb
 import duplicatedlog_pb2_grpc as my_grpc
 
+app = Flask(__name__)
+api = Api(app)
+
 logging.basicConfig(format='%(asctime)s: : %(message)s', level=logging.DEBUG)
 log = logging.getLogger(__name__)
-protocol = Protocol()
+
+class SecondaryProtocol(my_grpc.ProtocolServicer):
+    def replicate(self, request, context):
+        log.info(f"Received: {request.message}")
+        messages[str(uuid.uuid4())] = request.message
+        return my_pb.Response(message="ok")
+
+class MessageApi(Resource):
+    def get(self):
+        return messages
+
+api.add_resource(MessageApi, '/')
+protocol = SecondaryProtocol()
 nodeid = uuid.uuid4()
+messages = {} 
+masterPort = 50051
 
 def add_node():
     log.info("Registering secondary node ...")
-    with grpc.insecure_channel("localhost:50051") as channel:
+    with grpc.insecure_channel("localhost:" + str(masterPort)) as channel:
         stub = my_grpc.ProtocolStub(channel)
         response = stub.register_secondary_node(my_pb.Request(message=str(nodeid)))
     log.info(f"Registered on port: {response.message}")
     return response.message
 
 def serve():
-    port = add_node()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     my_grpc.add_ProtocolServicer_to_server(protocol, server)
     server.add_insecure_port("[::]:" + str(port))
@@ -29,5 +47,10 @@ def serve():
     log.info(f"Server started, listening on port: {port}")
     server.wait_for_termination()
 
+def web():
+    app.run(debug=True, use_reloader=False, port=port)
+
 if __name__ == "__main__":
+    port = add_node()
+    Thread(target=web).start()
     serve()
