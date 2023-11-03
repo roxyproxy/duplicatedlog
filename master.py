afb1983ebc4c42c4e2c4f9e812b67d64690bf883
Roksolana Povalii <rpovalii@tripadvisor.com>
2023-11-03 12:09:09 +0200

    added docker compose

--- master.py
+++ master.py
@@ -7,8 +7,8 @@ import logging
 import grpc
 import duplicatedlog_pb2 as my_pb   
 import duplicatedlog_pb2_grpc as my_grpc
-import time
 import uuid
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,16 +17,14 @@ logging.basicConfig(format='%(asctime)s: : %(message)s', level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 messages = {}
-masterPort = 50051
 
 class MasterProtocol(my_grpc.ProtocolServicer):
     nodes = {}
-    port = masterPort
+    port = os.getenv('MASTER_PORT')
 
     def register_secondary_node(self, request, context):
-        self.port += 1
-        self.nodes[request.message] = self.port
-        log.info(f"Registering secondary node on port: {self.port}")
+        self.nodes[request.message] = ""
+        log.info(f"Registering secondary node: {request.message}")
         return my_pb.Response(message=str(self.port))
 
 class MessageApi(Resource):
@@ -37,26 +35,19 @@ class MessageApi(Resource):
         key = str(uuid.uuid4())
         messages[key] = request.form['message']
         replicate(request.form['message'])
-        return key
+        return "ok"
 
 api.add_resource(MessageApi, '/')
 protocol = MasterProtocol()
 
 def replicate(msg):
     log.info(f"Sending a message ... {msg}")
-    messages[str(uuid.uuid4())] = msg
 
     for key in protocol.nodes:
-        with grpc.insecure_channel("localhost:" + str(protocol.nodes[key])) as channel:
+        with grpc.insecure_channel(key) as channel:
             stub = my_grpc.ProtocolStub(channel)
             response = stub.replicate(my_pb.Request(message=msg))
         log.info(f"Response from {key}: {response.message}")
-
-def test_replication():
-    # test message replication
-    for count in range(5):
-        time.sleep(3)
-        replicate("message" + str(count))
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
@@ -67,12 +58,8 @@ def serve():
     server.wait_for_termination()
 
 def web():
-    app.run(debug=True, use_reloader=False, port=masterPort)
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8050)
 
 if __name__ == "__main__":
     Thread(target=web).start()
-    # Thread(target=test_replication).start()
     serve()
-
-
-
--- secondary.py
+++ secondary.py
@@ -8,6 +8,8 @@ import uuid
 import grpc
 import duplicatedlog_pb2 as my_pb
 import duplicatedlog_pb2_grpc as my_grpc
+import os
+import time
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,6 +19,7 @@ log = logging.getLogger(__name__)
 
 class SecondaryProtocol(my_grpc.ProtocolServicer):
     def replicate(self, request, context):
+        time.sleep(int(os.getenv('DELAY')))
         log.info(f"Received: {request.message}")
         messages[str(uuid.uuid4())] = request.message
         return my_pb.Response(message="ok")
@@ -29,28 +32,26 @@ api.add_resource(MessageApi, '/')
 protocol = SecondaryProtocol()
 nodeid = uuid.uuid4()
 messages = {} 
-masterPort = 50051
 
 def add_node():
-    log.info("Registering secondary node ...")
-    with grpc.insecure_channel("localhost:" + str(masterPort)) as channel:
+    with grpc.insecure_channel(os.getenv('MASTER_HOST') + ":" + os.getenv('MASTER_PORT')) as channel:
         stub = my_grpc.ProtocolStub(channel)
-        response = stub.register_secondary_node(my_pb.Request(message=str(nodeid)))
-    log.info(f"Registered on port: {response.message}")
+        response = stub.register_secondary_node(my_pb.Request(message=str(os.getenv('SECONDARY_HOST') + ":" + os.getenv('SECONDARY_PORT'))))
+    log.info(f"Registered secondary node: {os.getenv('SECONDARY_HOST')}:{os.getenv('SECONDARY_PORT')}")
     return response.message
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     my_grpc.add_ProtocolServicer_to_server(protocol, server)
-    server.add_insecure_port("[::]:" + str(port))
+    server.add_insecure_port(os.getenv('SECONDARY_HOST') + ":" + os.getenv('SECONDARY_PORT'))
     server.start()
-    log.info(f"Server started, listening on port: {port}")
+    log.info(f"Secondary node started: {os.getenv('SECONDARY_HOST')}:{os.getenv('SECONDARY_PORT')}")
     server.wait_for_termination()
 
 def web():
-    app.run(debug=True, use_reloader=False, port=port)
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8051)
 
 if __name__ == "__main__":
-    port = add_node()
+    nodeid = add_node()
     Thread(target=web).start()
     serve()
